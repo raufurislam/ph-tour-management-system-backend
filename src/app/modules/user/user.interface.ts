475ddc1650e767ad1124ac4202aa7709545620f2
Raufur Islam <raufurislam@gmail.com>
2025-07-11 17:28:50 +0600

    User interface, model and controller

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,39 @@
+import { Types } from "mongoose";
+
+export enum Role {
+  SUPER_ADMIN = "SUPER_ADMIN",
+  ADMIN = "ADMIN",
+  USER = "USER",
+  GUILD = "GUILD",
+}
+
+// auth providers
+// email, password
+// google authentication
+
+export interface IAuthProvider {
+  provider: string;
+  providerId: string;
+}
+
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCK",
+}
+
+export interface IUser {
+  name: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  picture?: string;
+  address?: string;
+  isDeleted?: string;
+  isActive?: IsActive;
+  isVerified?: string;
+  role: Role;
+  auths: IAuthProvider[];
+  bookings?: Types.ObjectId[];
+  guides?: Types.ObjectId[];
+}
