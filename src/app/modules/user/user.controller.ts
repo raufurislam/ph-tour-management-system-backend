5ed6c8d48da5a63f3395db7faf7ec9a30aaba190
Raufur Islam <raufurislam@gmail.com>
2025-07-12 15:41:48 +0600

    catchAsync higer order function for try catch function

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,28 +1,55 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // user.controller.ts
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
-import { UserServices } from "./user.service";
 // import AppError from "../../errorHelpers/AppError";
+import { UserServices } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    // throw new Error("Fake error");
-    // throw new AppError(httpStatus.BAD_REQUEST, "Fake error");
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     // throw new Error("Fake error");
+//     // throw new AppError(httpStatus.BAD_REQUEST, "Fake error");
+//     const user = await UserServices.createUser(req.body);
+
+//     res.status(httpStatus.CREATED).json({
+//       message: "User created Successfully",
+//       user,
+//     });
+//   } catch (err: any) {
+//     console.log(err);
+//     next(err);
+//   }
+// };
+
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body);
 
     res.status(httpStatus.CREATED).json({
-      message: "User created Successfully",
+      message: "User Created Successfully",
       user,
     });
-  } catch (err: any) {
-    console.log(err);
-    next(err);
   }
-};
+);
+
+const getAllUsers = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const users = await UserServices.getAllUsers();
+    res.status(httpStatus.OK).json({
+      success: true,
+      message: "All users retrieve successfully",
+      data: users,
+    });
+  }
+);
+
+// function → res-res function
 
 export const UserController = {
   createUser,
+  getAllUsers,
 };
 
 // Route matching (app → index → user.route) → controller → service → model → DB
