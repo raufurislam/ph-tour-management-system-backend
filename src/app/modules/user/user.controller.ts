6509e4848fb97e8c33eb4cea0d3c3555e1a7caff
Raufur Islam <raufurislam@gmail.com>
2025-07-12 14:45:37 +0600

    Custom error handler

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,10 +3,12 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
+// import AppError from "../../errorHelpers/AppError";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     // throw new Error("Fake error");
+    // throw new AppError(httpStatus.BAD_REQUEST, "Fake error");
     const user = await UserServices.createUser(req.body);
 
     res.status(httpStatus.CREATED).json({
