6d8b769f0e9863d273cde8b63d9d90d6dda3ca9a
Raufur Islam <raufurislam@gmail.com>
2025-07-15 01:52:05 +0600

    Jwt verified token and protected route using middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,11 @@
 // user.route.ts
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { UserControllers } from "./user.controller";
 import { createUserZodSchema } from "./user.validation";
 import { validateRequest } from "../../middlewares/validateRequest";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import AppError from "../../errorHelpers/AppError";
+import { Role } from "./user.interface";
 
 const router = Router();
 
@@ -12,6 +15,34 @@ router.post(
   UserControllers.createUser
 );
 
-router.get("/all-users", UserControllers.getAllUsers);
+router.get(
+  "/all-users",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const accessToken = req.headers.authorization;
+
+      if (!accessToken) {
+        throw new AppError(403, "No token found");
+      }
+
+      const verifiedToken = jwt.verify(accessToken, "secret");
+
+      // if (!verifiedToken) {
+      //   console.log(verifiedToken);
+      //   throw new AppError(403, `You are not authorized ${verifiedToken}`);
+      // }
+
+      if ((verifiedToken as JwtPayload).role !== Role.ADMIN) {
+        throw new AppError(403, "You are not permitted to view this route");
+      }
+      console.log(verifiedToken);
+      next();
+    } catch (error) {
+      // console.log("Jwt error", error);
+      next(error);
+    }
+  },
+  UserControllers.getAllUsers
+);
 
 export const UserRoutes = router;
