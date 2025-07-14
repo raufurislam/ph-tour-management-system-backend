575318edf5eaaa250d6f7632f0dfabfb731edb1c
Raufur Islam <raufurislam@gmail.com>
2025-07-15 00:44:05 +0600

    User login api

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,1 +1,33 @@
-// const credentialLogin
+// auth.service.ts
+import AppError from "../../errorHelpers/AppError";
+import { IUser } from "../user/user.interface";
+import httpStatus from "http-status-codes";
+import { User } from "../user/user.model";
+import bcryptjs from "bcryptjs";
+
+const credentialLogin = async (payload: Partial<IUser>) => {
+  const { email, password } = payload;
+
+  const isUserExist = await User.findOne({ email });
+
+  if (!isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist");
+  }
+
+  const isPasswordMatch = await bcryptjs.compare(
+    password as string,
+    isUserExist.password as string
+  );
+
+  if (!isPasswordMatch) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Incorrect password");
+  }
+
+  return {
+    email: isUserExist.email,
+  };
+};
+
+export const AuthServices = {
+  credentialLogin,
+};
