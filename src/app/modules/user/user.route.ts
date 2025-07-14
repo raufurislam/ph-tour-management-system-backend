85fe67aed9f62be989333ae426e18e0135a9810a
Raufur Islam <raufurislam@gmail.com>
2025-07-14 16:21:21 +0600

    Zod validation api and validateRequest middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,56 +1,17 @@
 // user.route.ts
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import { UserControllers } from "./user.controller";
-import z, { optional } from "zod";
+import { createUserZodSchema } from "./user.validation";
+import { validateRequest } from "../../middlewares/validateRequest";
 
 const router = Router();
 
 router.post(
   "/register",
-  async (req: Request, res: Response, next: NextFunction) => {
-    const createUserZodSchema = z.object({
-      name: z
-        .string({ invalid_type_error: "Name must be string" })
-        .min(2, { message: "Name must be at least 2 characters long." })
-        .max(50, { message: "Name cannot exceed 50 characters." }),
-      email: z
-        .string({ invalid_type_error: "Email must be string" })
-        .email({ message: "Invalid email address format." })
-        .min(5, { message: "Email must be at least 5 characters long." })
-        .max(100, { message: "Email cannot exceed 100 characters." }),
-      password: z
-        .string({ invalid_type_error: "Password must be string" })
-        .min(8, { message: "Password must be at least 8 characters long." })
-        .regex(/^(?=.*[A-Z])/, {
-          message: "Password must contain at least 1 uppercase letter.",
-        })
-        .regex(/^(?=.*[!@#$%^&*])/, {
-          message: "Password must contain at least 1 special character.",
-        })
-        .regex(/^(?=.*\d)/, {
-          message: "Password must contain at least 1 number.",
-        }),
-      phone: z
-        .string({ invalid_type_error: "Phone Number must be string" })
-        .regex(/^(?:\+8801\d{9}|01\d{9})$/, {
-          message:
-            "Phone number must be valid for Bangladesh. Format: +8801XXXXXXXXX or 01XXXXXXXXX",
-        })
-        .optional(),
-      address: z
-        .string({ invalid_type_error: "Address must be string" })
-        .max(200, { message: "Address cannot exceed 200 characters." })
-        .optional(),
-    });
-
-    req.body = await createUserZodSchema.parseAsync(req.body);
-
-    console.log(req.body);
-
-    // next();
-  },
+  validateRequest(createUserZodSchema),
   UserControllers.createUser
 );
+
 router.get("/all-users", UserControllers.getAllUsers);
 
 export const UserRoutes = router;
