caf7f24650cc450f5ce445629bba1b37b3b1f437
Raufur Islam <raufurislam@gmail.com>
2025-07-15 21:37:06 +0600

    Super Admin created

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
 import { envVars } from "./app/config/env";
+import { seedSuperAdmin } from "./app/utils/seedSuperAdmin";
 
 // let myAge;
 
@@ -21,8 +22,10 @@ const startServer = async () => {
   }
 };
 
-startServer();
-// seedSuperAdmin()
+(async () => {
+  await startServer();
+  await seedSuperAdmin();
+})();
 
 process.on("unhandledRejection", (err) => {
   console.log("Unhandled Rejection detected. Sever shutting down...", err);
