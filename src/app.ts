d551ec0e33f5008afd752f02480b42ec960b107f
Raufur Islam <raufurislam@gmail.com>
2025-07-17 23:58:26 +0600

    Cookie parser refresh token

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import cors from "cors";
 import { router } from "./app/routes";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 
 app.use("/api/v1", router);
