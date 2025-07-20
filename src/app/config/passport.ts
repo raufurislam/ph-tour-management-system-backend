6388f5cc6fbc5f3aa4360ed4f8aa8d47cfaabd29
Raufur Islam <raufurislam@gmail.com>
2025-07-20 17:48:52 +0600

    Complete google authentication configaration

--- src/app/config/passport.ts
+++ src/app/config/passport.ts
@@ -1,6 +1,12 @@
 import passport from "passport";
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import {
+  Strategy as GoogleStrategy,
+  Profile,
+  VerifyCallback,
+} from "passport-google-oauth20";
 import { envVars } from "./env";
+import { User } from "../modules/user/user.model";
+import { Role } from "../modules/user/user.interface";
 
 passport.use(
   new GoogleStrategy(
@@ -9,6 +15,48 @@ passport.use(
       clientSecret: envVars.GOOGLE_CLIENT_SECRET,
       callbackURL: envVars.GOOGLE_CALLBACK_URL,
     },
-    async () => {}
+    async (
+      accessToken: string,
+      refreshToken: string,
+      profile: Profile,
+      done: VerifyCallback
+    ) => {
+      try {
+        const email = profile.emails?.[0].value;
+
+        if (!email) {
+          return done(null, false, { message: " No email found" });
+        }
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+          user = await User.create({
+            email,
+            name: profile.displayName,
+            picture: profile.photos?.[0].value,
+            role: Role.USER,
+            isVerified: true,
+            auths: [
+              {
+                provider: "google",
+                providerId: profile.id,
+              },
+            ],
+          });
+        }
+
+        return done(null, user);
+      } catch (error) {
+        console.log("Google strategy error", error);
+        return done(error);
+      }
+    }
   )
 );
+
+// frontend localhost:5173/login?redirect=/booking -> localhost:5000/api/v1/auth/google?redirect=/booking -> passport -> Google OAuth Consent -> gmail login -> successful -> callback url localhost:5000/api/v1/auth/google/callback -> db store -> token
+
+// Bridge == Google -> user db store -> token
+//Custom -> email , password, role : USER, name... -> registration -> DB -> 1 User create
+//Google -> req -> google -> successful : Jwt Token : Role , email -> DB - Store -> token - api access
