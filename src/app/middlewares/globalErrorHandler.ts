4b50ac5e9d4335acef5a4bdd437b1a400a8f76e6
Raufur Islam <raufurislam@gmail.com>
2025-07-21 13:08:59 +0600

    29-6 Handling Mongoose Validation Error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -3,6 +3,7 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
 import AppError from "../errorHelpers/AppError";
+import { object } from "zod";
 
 export const globalErrorHandler = (
   err: any,
@@ -10,7 +11,13 @@ export const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log(err);
+  // console.log(err);
+  const errorSources: any = [
+    // {
+    //   path: "isDeleted",
+    //   message: "Cast failed",
+    // },
+  ];
   let statusCode = 500;
   let message = "Something Went Wrong!!";
 
@@ -29,6 +36,20 @@ export const globalErrorHandler = (
   else if (err.name === "CastError") {
     statusCode = 400;
     message = "Invalid MongoDB ObjectID. Please provide a valid id";
+  }
+
+  // ValidationError
+  else if (err.name === "ValidationError") {
+    statusCode = 400;
+    const errors = Object.values(err.errors);
+
+    errors.forEach((errorObject: any) =>
+      errorSources.push({
+        path: errorObject.path,
+        message: errorObject.message,
+      })
+    );
+    message = "Validation Error";
   } else if (err instanceof AppError) {
     statusCode = err.statusCode;
     message = err.message;
@@ -40,6 +61,7 @@ export const globalErrorHandler = (
   res.status(statusCode).json({
     success: false,
     message,
+    errorSources,
     err,
     stack: envVars.NODE_ENV === "development" ? err.stack : null,
   });
