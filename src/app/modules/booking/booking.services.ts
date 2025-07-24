22bb9163e698aebaf811641c8cac1dd0a1f91302
Raufur Islam <raufurislam@gmail.com>
2025-07-25 04:03:46 +0600

    31-5 Intro to SSL Commerz , Account Creation Process

--- src/app/modules/booking/booking.services.ts
+++ src/app/modules/booking/booking.services.ts
@@ -84,6 +84,10 @@ const getAllBookings = async () => {
   return {};
 };
 
+// Frontend(localhost:5173) - User - Tour - Booking (Pending) - Payment(Unpaid) -> SSLCommerz Page -> Payment Complete -> Backend(localhost:5000/api/v1/payment/success) -> Update Payment(PAID) & Booking(CONFIRM) -> redirect to frontend -> Frontend(localhost:5173/payment/success)
+
+// Frontend(localhost:5173) - User - Tour - Booking (Pending) - Payment(Unpaid) -> SSLCommerz Page -> Payment Fail / Cancel -> Backend(localhost:5000) -> Update Payment(FAIL / CANCEL) & Booking(FAIL / CANCEL) -> redirect to frontend -> Frontend(localhost:5173/payment/cancel or localhost:5173/payment/fail)
+
 const getUserBookings = async () => {
   return {};
 };
