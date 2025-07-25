c6f42443744326544a75200cd268bc5b5aba2a32
Raufur Islam <raufurislam@gmail.com>
2025-07-25 23:01:00 +0600

    32-1 Introduction to Multer and Cloudinary, Opening Cloudinary account and get secrets

---
+++ src/app/config/cloudinary.config.ts
@@ -0,0 +1,5 @@
+// Frontend -> Form Data with Image File -> Multer -> Form data -> Req (Body + File)
+// Amader folder -> image -> form data -> File -> Multer -> Amader project / pc te Nijer ekta folder(temporary) -> Req.file
+
+//req.file -> cloudinary(req.file) -> url -> mongoose -> mongodb
+//Amader folder -> image -> form data -> File -> Multer -> storage in cloudinary -> url ->  req.file  -> url  -> mongoose -> mongodb
