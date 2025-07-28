5638f385d68a309d524a17bab55edbb1ad5dbaee
Raufur Islam <raufurislam@gmail.com>
2025-07-28 23:05:41 +0600

    38.9 init

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -16,13 +16,21 @@ router.post(
   checkAuth(...Object.values(Role)),
   AuthControllers.changePassword
 );
+
 router.post(
   "/reset-password",
   checkAuth(...Object.values(Role)),
   AuthControllers.resetPassword
 );
+
 router.post(
   "/set-password",
+  checkAuth(...Object.values(Role)),
+  AuthControllers.setPassword
+);
+
+router.post(
+  "/forgot-password",
   checkAuth(...Object.values(Role)),
   AuthControllers.setPassword
 );
