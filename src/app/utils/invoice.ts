e48df777e19f9cd6c04dbbedbf1bd78c9c0d9e81
Raufur Islam <raufurislam@gmail.com>
2025-08-08 21:37:46 +0600

    33-4 Create a function to generate pdf of the invoice of booking and payment of a tour

---
+++ src/app/utils/invoice.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import PDFDocument from "pdfkit";
+import AppError from "../errorHelpers/AppError";
+
+export interface IInvoiceData {
+  transactionId: string;
+  bookingDate: Date;
+  userName: string;
+  tourTitle: string;
+  guestCount: string;
+  totalAmount: number;
+}
+
+export const generatePdf = async (invoiceData: IInvoiceData) => {
+  try {
+    return new Promise((resolve, reject) => {
+      const doc = new PDFDocument({ size: "A4", margin: 50 });
+
+      const buffer: Uint8Array[] = [];
+
+      doc.on("data", (chunk) => buffer.push(chunk));
+      doc.on("end", () => resolve(Buffer.concat(buffer)));
+      doc.on("error", (err) => reject(err));
+
+      //   PDF content
+      doc.fontSize(20).text("Invoice", { align: "center" });
+      doc.moveDown();
+      doc.fontSize(14).text(`Transaction Id: ${invoiceData.transactionId}`);
+      doc.text(`Booking Date: ${invoiceData.bookingDate}`);
+      doc.text(`Customer: ${invoiceData.userName}`);
+      doc.moveDown();
+
+      doc.text(`Tour: ${invoiceData.tourTitle}`);
+      doc.text(`Guests: ${invoiceData.guestCount}`);
+      doc.text(`Total Amount: $${invoiceData.totalAmount.toFixed(2)}`);
+      doc.moveDown();
+      doc.end();
+    });
+  } catch (error: any) {
+    console.log(error);
+    throw new AppError(401, `PDF creation error ${error.message}`);
+  }
+};
