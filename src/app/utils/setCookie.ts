0b377f6bfdee3a8bd1157581349c2c981d589e8a
Raufur Islam <raufurislam@gmail.com>
2025-08-12 23:23:52 +0600

    Postman api json

--- src/app/utils/setCookie.ts
+++ src/app/utils/setCookie.ts
@@ -18,6 +18,7 @@ export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
 
   if (tokenInfo.refreshToken) {
     res.cookie("refreshToken", tokenInfo.refreshToken, {
+      httpOnly: true,
       secure: envVars.NODE_ENV === "production",
       sameSite: "none",
     });
